40b7e7bea49679514595aa257906d7a5562c1e1d
punpundepgaivl <ngthchant@gmail.com>
2024-04-20 21:49:57 +0700

    Add Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.circleindicator)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
